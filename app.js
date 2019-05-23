5b14f4c6c6aa9ccaf192085e19dbe43dc3a8075c
Jacky Wong <jackyw2017@gmail.com>
2019-05-23 10:00:13 -0400

    fetchData function done

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 // const express = require('express');
 const Twitter = require('twitter');
 const fs = require('fs');
-// const app = express();
+// const app = express();`
 
-// Change out keys here:
-const client = new Twitter({
-  consumer_key: 'dTSiKVlAc4pSKVTKyejgpszRL',
-  consumer_secret: 'lSbNSmj0MJ8sJULpI0zJolYLEIk9f4BYFSvPaiEI0lsYTYWpIU',
-  access_token_key: '1130184699857522693-KGIrOBJTh8enA4lh3VJFCrSgoRZNVY',
-  access_token_secret: 'ixHm5MLcZuCalft1MuBXxy2xgTSo8GH7sYZDMt9zq8bJj',
-});
+// Create a new Twitter object with consumer_key, consumer_secret, access_token_key, access_token_secret in ./.env.js
+const client = require('./.env.js');
+
+const fetchData = (endPoint, parameterObj) => {
+  client.get(endPoint, parameterObj, (error, data, response) => {
+    if (error) console.log(error);
+
+    let dataJSON = JSON.stringify(data);
+    const fileName = endPoint.split('/').join('') + '.json';
+
+    fs.writeFile(fileName, dataJSON, 'utf8', err => {
+      if (err) {
+        console.log('An error occured while writing JSON');
+        return console.log(err);
+      }
+
+      console.log('JSON file has been saved');
+    });
+  });
+};
+
+fetchData('search/tweets', { q: '#mcdonalds' });
 
 // client.get('search/tweets', { q: '#mcdonalds' }, (error, tweets, response) => {
 //   if (error) console.log(error);
@@ -45,6 +60,6 @@ const client = new Twitter({
 //   }
 // );
 
-const timeline = JSON.parse(require('./statuses.userTimeline.json'));
+// const timeline = JSON.parse(require('./statuses.userTimeline.json'));
 
-console.log(timeline.length);
+// console.log(timeline.length);
